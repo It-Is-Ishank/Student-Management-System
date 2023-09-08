df5d7a6bc4fe596f700672e75460b2e8df948ab9
It-Is-Ishank <102756532+It-Is-Ishank@users.noreply.github.com>
2023-09-08 12:35:46 +0530

    Update SMS.c to handle names with spaces

--- SMS.c
+++ SMS.c
@@ -1,384 +1,271 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include <conio.h>
-#include <Windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
-//void displayStudents(void);
-//void searchStudent(void);
-//void modifyStudent(void);
-//void deleteStudent(void);
+#define MAX_STUDENTS 100
 
 struct Student {
-    int SID; // Student ID
-    char name[50]; // Student name
-    float CGPA; // Cumulative Grade Point Average
-    int currSem; // Current semester
+    int SID;
+    char name[50];
+    float CGPA;
+    int currSem;
 };
 
+void addStudent(struct Student* studentArray[], int* studentCount) {
+    if (*studentCount < MAX_STUDENTS) {
+        struct Student* newStudent = (struct Student*)malloc(sizeof(struct Student));
+        
+        // Get student data from user input
+        printf("Enter Student ID: ");
+        scanf("%d", &newStudent->SID);
+        printf("Enter Student Name: ");
+        scanf(" %[^\n]s", newStudent->name); // Read the entire line, including spaces
+        printf("Enter CGPA: ");
+        scanf("%f", &newStudent->CGPA);
+        printf("Enter Current Semester: ");
+        scanf("%d", &newStudent->currSem);
+
+        // Add the new student to the array
+        studentArray[*studentCount] = newStudent;
+        (*studentCount)++;
+    } else {
+        printf("Student limit reached.\n");
+    }
+}
+int compareStudentID(const void* a, const void* b) {
+    const struct Student* studentA = *(const struct Student**)a;
+    const struct Student* studentB = *(const struct Student**)b;
+    
+    return studentA->SID - studentB->SID;
+}
+
+void deleteStudent(struct Student* studentArray[], int* studentCount, int targetSID) {
+    int indexToDelete = -1;
+
+    // Find the index of the student with the target SID
+    for (int i = 0; i < *studentCount; i++) {
+        if (studentArray[i]->SID == targetSID) {
+            indexToDelete = i;
+            break;
+        }
+    }
+
+    if (indexToDelete == -1) {
+        printf("Student with SID %d not found.\n", targetSID);
+        return;
+    }
+
+    // Free the memory allocated for the student to be deleted
+    free(studentArray[indexToDelete]);
+
+    // Move the remaining students to fill the gap
+    for (int i = indexToDelete; i < *studentCount - 1; i++) {
+        studentArray[i] = studentArray[i + 1];
+    }
+
+    // Decrement the student count
+    (*studentCount)--;
+
+    printf("Student with SID %d deleted successfully.\n", targetSID);
+}
+
+void saveStudents(struct Student* studentArray[], int studentCount) {
+
+    qsort(studentArray, studentCount, sizeof(struct Student*), compareStudentID);
+
+    FILE* file = fopen("SDB.txt", "w");
+    if (file == NULL) {
+        printf("Error opening file.\n");
+        return;
+    }
+
+    // Save the student count as the first line
+    fprintf(file, "%d\n", studentCount);
+
+    for (int i = 0; i < studentCount; i++) {
+        // Save each student's data, separated by a delimiter character (e.g., ';')
+        fprintf(file, "%d; %s; %.2f; %d\n", studentArray[i]->SID, studentArray[i]->name, studentArray[i]->CGPA, studentArray[i]->currSem);
+    }
+
+    fclose(file);
+}
+
+
+void loadStudents(struct Student* studentArray[], int* studentCount) {
+    FILE* file = fopen("SDB.txt", "r");
+    if (file == NULL) {
+        printf("Error opening file.\n");
+        return;
+    }
+
+    // Read the student count from the first line
+    if (fscanf(file, "%d", studentCount) != 1) {
+        printf("Error reading student count.\n");
+        fclose(file);
+        return;
+    }
+
+    // Consume the newline character after the student count
+    int newlineChar = fgetc(file);
+
+    for (int i = 0; i < *studentCount; i++) {
+        // Allocate memory for a new student object
+        studentArray[i] = (struct Student*)malloc(sizeof(struct Student));
+
+        // Read each field, separating them by a delimiter character (e.g., ';')
+        if (fscanf(file, "%d", &studentArray[i]->SID) != 1 ||
+            fscanf(file, "; %49[^;]", studentArray[i]->name) != 1 ||
+            fscanf(file, "; %f", &studentArray[i]->CGPA) != 1 ||
+            fscanf(file, "; %d", &studentArray[i]->currSem) != 1) {
+            printf("Error reading student data.\n");
+            break;
+        }
+    }
+
+    fclose(file);
+}
+
+
+
+
+
+
+
+
+
+
+void clearScreen() {
+    printf("\033[H\033[J");
+}
 
 void gotoxy(int x, int y) {
-    COORD coord;
-    coord.X = x;
-    coord.Y = y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+    printf("\033[%d;%dH", y, x);
 }
 
-int isDuplicateSID(int sidToCheck) {
-    struct Student s;
-    FILE *file = fopen("SDB.txt", "r");
-    
-    if (file == NULL) {
-        // If the file doesn't exist, create it
-        file = fopen("SDB.txt", "w");
-        if (file == NULL) {
-            printf("Error creating file.\n");
-            return 0; 
-        } else {
-            fclose(file);
-            return 0; // File is newly created, so no duplicates
-        }
+// Function to display students in a tabular format on Windows
+// Function to display students in a tabular format
+void displayStudentsTable(struct Student* studentArray[], int studentCount) {
+    clearScreen(); // Clear the terminal screen
+
+    // Define table headers
+    printf("Student ID\tName\t\t   CGPA\t\tSemester\n");
+
+    for (int i = 0; i < studentCount; i++) {
+        // Set cursor position for each row
+        int row = i + 3; // Start from the fourth row
+        gotoxy(1, row);
+
+        // Display student data with a fixed tab width for the "Name" column
+        printf("%d\t\t%-20s%.2f\t%d\n", studentArray[i]->SID, studentArray[i]->name, studentArray[i]->CGPA, studentArray[i]->currSem);
     }
 
-    while (fscanf(file, "%d %*s %*f %*d", &s.SID) != EOF) {
-        if (s.SID == sidToCheck) {
-            fclose(file);
-            return 1; // Duplicate SID found
-        }
-    }
-
-    fclose(file);
-    return 0; // No duplicate SID found
+    // Move cursor to a new line
+    int newRow = studentCount + 4; // Move to the next line after the table
+    gotoxy(1, newRow);
 }
 
-void insertStudent(struct Student student) {
-    struct Student s;
-    FILE *file = fopen("SDB.txt", "r+");
-    
-    if (file == NULL) {
-        // If the file doesn't exist, create it
-        file = fopen("SDB.txt", "w");
-        if (file == NULL) {
-            printf("Error creating file.\n");
-            return;
-        }
-    }
+void modifyStudent(struct Student* studentArray[], int studentCount, int targetSID) {
+    displayStudentsTable(studentArray,studentCount);
+    int found = 0;
 
-    while (fscanf(file, "%d %*s %*f %*d", &s.SID) != EOF) {
-        if (s.SID > student.SID) {
-            // Move the file pointer back to the beginning of the line
-            fseek(file, -14, SEEK_CUR); // Adjust based on your data format
-            break;
-        }
-    }
-
-    // Insert the student at the correct position
-    fprintf(file, "%d %s %.2f %d\n", student.SID, student.name, student.CGPA, student.currSem);
-
-    fclose(file);
-}
-
-int getPositiveIntInput(const char* prompt) {
-    int value;
-    do {
-        printf("%s", prompt);
-        if (scanf("%d", &value) != 1 || value <= 0) {
-            printf("Invalid input. Please enter a positive integer.\n");
-            while (getchar() != '\n'); // Clear input buffer
-        } else {
-            break;
-        }
-    } while (1);
-    return value;
-}
-
-// Function to validate and get a float input
-float getFloatInput(const char* prompt) {
-    float value;
-    do {
-        printf("%s", prompt);
-        if (scanf("%f", &value) != 1) {
-            printf("Invalid input. Please enter a valid float value.\n");
-            while (getchar() != '\n'); // Clear input buffer
-        } else {
-            break;
-        }
-    } while (1);
-    return value;
-}
-
-
-void addStudent() {
-    struct Student student;
-
-    system("cls");
-
-    printf("Add Student:\n");
-    student.SID = getPositiveIntInput("Enter Student ID: ");
-
-    // Check for duplicate SID
-    if (isDuplicateSID(student.SID)) {
-        printf("Student with the same ID already exists.\n");
-        printf("Press any key to continue...");
-        _getch();
-        return;
-    }
-
-    printf("Enter student name: ");
-    scanf("%s", student.name); // Use %s for reading names without spaces
-    student.CGPA = getFloatInput("Enter CGPA: ");
-      student.currSem = getPositiveIntInput("Enter current semester: ");
-
-    // Insert the student in sorted order
-    insertStudent(student);
-
-    printf("Student added successfully!\n");
-    printf("Press any key to continue...");
-    _getch();
-}
-
-
-void displayStudents() {
-    struct Student s;
-
-    system("cls");
-
-    FILE *file = fopen("SDB.txt", "r");
-    if (file == NULL) {
-        printf("Error opening file.\n");
-        printf("Press any key to continue...");
-        _getch();
-        return;
-    }
-
-    // Print column headers using gotoxy
-    gotoxy(5, 1);
-    printf("Student ID");
-    gotoxy(20, 1);
-    printf("Name");
-    gotoxy(35, 1);
-    printf("CGPA");
-    gotoxy(50, 1);
-    printf("Current Semester");
-
-    int row = 3; // Starting row for data
-
-    while (fscanf(file, "%d %s %f %d", &s.SID, &s.name, &s.CGPA, &s.currSem) != EOF) {
-        // Print data using gotoxy
-        gotoxy(5, row);
-        printf("%d", s.SID);
-        gotoxy(20, row);
-        printf("%s", s.name);
-        gotoxy(35, row);
-        printf("%.2f", s.CGPA);
-        gotoxy(50, row);
-        printf("%d", s.currSem);
-
-        row++; // Move to the next row
-    }
-
-    fclose(file);
-
-    // Wait for a keypress and return to the main menu
-    gotoxy(0, row + 1);
-    
-}
-
-
-void searchStudent(){
-    int targetSId;
-    struct Student s;
-
-    system("cls");
-
-    s.SID = getPositiveIntInput("Enter SID to Search: ");
-
-    FILE *file = fopen("SDB.txt","r");
-    if (file == NULL ){
-        printf("\nError opening the file.\n");
-        return;
-    }
-
-    int flag=0;
-    while(fscanf(file,"%d %s %f %d",&s.SID,&s.name,&s.CGPA,&s.currSem) != EOF){
-        if(s.SID == targetSId){
-            flag =1;
-            printf("Student Found.\n");
-            printf("Student ID: %d\n", s.SID);
-            printf("Name: %s\n", s.name);
-            printf("CGPA: %.2f\n", s.CGPA);
-            printf("Current Semester: %d\n", s.currSem);
-            break;
-        }
-    }
-    if(!flag){
-        printf("Student with id %d not found in DB.\n",targetSId);
-    }
-
-    fclose(file);
-
-    printf("Press any key to continue...");
-    _getch();
-
-
-
-};
-
-void modifyStudent(){
-    int targetSID;
-    struct Student student;
-
-    system("cls");
-
-    displayStudents();
-
-    targetSID = getPositiveIntInput("Enter SID to Modify Data: ");
-    
-    FILE* file = fopen("SDB.txt", "r");
-    if (file == NULL) {
-        printf("Error opening file.\n");
-        return;
-    }
-
-    FILE* tempFile = fopen("temp_database.txt", "w");
-    if (tempFile == NULL) {
-        printf("Error creating temporary file.\n");
-        fclose(file);
-        return;
-    }
-
-    int found = 0; // To track if the student was found
-    while (fscanf(file, "%d %s %f %d", &student.SID, student.name, &student.CGPA, &student.currSem) != EOF) {
-        if (student.SID == targetSID) {
+    // Search for the student with the target SID
+    for (int i = 0; i < studentCount; i++) {
+        if (studentArray[i]->SID == targetSID) {
             found = 1;
-            printf("Enter updated student name: ");
-            scanf("%s", student.name);
-            printf("Enter updated CGPA: ");
-            scanf("%f", &student.CGPA);
-            printf("Enter updated current semester: ");
-            scanf("%d", &student.currSem);
+            // Allow the user to modify the student's information
+            printf("Modifying student with SID %d:\n", targetSID);
+            printf("Enter new name: ");
+            scanf(" %[^\n]s", studentArray[i]->name);
+            printf("Enter new CGPA: ");
+            scanf("%f", &studentArray[i]->CGPA);
+            printf("Enter new semester: ");
+            scanf("%d", &studentArray[i]->currSem);
+            printf("Student information updated successfully.\n");
+            break;
         }
-        fprintf(tempFile, "%d %s %.2f %d\n", student.SID, student.name, student.CGPA, student.currSem);
     }
 
     if (!found) {
-        printf("Student with ID %d not found.\n", targetSID);
-    } else {
-        printf("Student information updated successfully!\n");
+        printf("Student with SID %d not found.\n", targetSID);
     }
-
-    fclose(file);
-    fclose(tempFile);
-
-    remove("SDB.txt");
-    rename("temp_database.txt", "SDB.txt");
-
-    printf("Press any key to continue...");
-    _getch();
-
-
-};
-
-void deleteStudent(){
-    int targetSID;
-    struct Student student;
-
-    system("cls");
-
-    displayStudents();
-
-
-    printf("Enter Student ID to delete: ");
-    scanf("%d", &targetSID);
-
-    FILE* file = fopen("SDB.txt", "r");
-    if (file == NULL) {
-        printf("Error opening file.\n");
-        return;
-    }
-
-    FILE* tempFile = fopen("temp_database.txt", "w");
-    if (tempFile == NULL) {
-        printf("Error creating temporary file.\n");
-        fclose(file);
-        return;
-    }
-
-    int found = 0; // To track if the student was found
-    while (fscanf(file, "%d %s %f %d", &student.SID, student.name, &student.CGPA, &student.currSem) != EOF) {
-        if (student.SID == targetSID) {
-            found = 1;
-            printf("Student with ID %d has been deleted.\n", targetSID);
-            continue; // Skip writing this student's data to the temporary file
-        }
-        fprintf(tempFile, "%d %s %.2f %d\n", student.SID, student.name, student.CGPA, student.currSem);
-    }
-
-    if (!found) {
-        printf("Student with ID %d not found.\n", targetSID);
-    }
-
-    fclose(file);
-    fclose(tempFile);
-
-    // Rename the temporary file to the original file
-    remove("SDB.txt");
-    rename("temp_database.txt", "SDB.txt");
-
-};
-
-
-
-
+}
 
 
 
 
 int main() {
+    struct Student* studentArray[MAX_STUDENTS];
+    int studentCount = 0;
+
+    loadStudents(studentArray, &studentCount); // Load student data from file
+
     int choice;
-
-    while (1) {
-        system("cls"); // Clear the console screen
-
-        printf("\nStudent Management System Menu:\n");
+    int target;
+    do {
+        printf("\nStudent Management System\n");
         printf("1. Add Student\n");
-        printf("2. Display Students\n");
-        printf("3. Search Student\n");
-        printf("4. Modify Student\n");
-        printf("5. Delete Student\n");
+        printf("2. View Students\n");
+        printf("3. Save Students\n");
+        printf("4. Delete a Student\n");
+        printf("5. Modify Details of a Student\n");
         printf("6. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
         switch (choice) {
             case 1:
-                addStudent();
+                if (studentCount < MAX_STUDENTS) {
+                    addStudent(studentArray, &studentCount);
+                } else {
+                    printf("Student limit reached.\n");
+                }
                 break;
             case 2:
-                displayStudents();
-                printf("Press any key to continue...");
-                _getch();
+                if (studentCount > 0) {
+                    displayStudentsTable(studentArray, studentCount);
+                    //pressAnyKeyToContinue(); // Wait for user input before returning to the menu
+                } else {
+                    printf("No students found.\n");
+                }
                 break;
             case 3:
-                searchStudent();
+                saveStudents(studentArray, studentCount); // Save students to file
+                printf("Student data saved successfully.\n");
+                //pressAnyKeyToContinue();
                 break;
             case 4:
-                modifyStudent();
+                displayStudentsTable(studentArray, studentCount);
+                
+                printf("\n\nEnter the SID to delete : ");
+                scanf("%d",&target);
+                deleteStudent(studentArray,&studentCount,target);
+                //pressAnyKeyToContinue();
                 break;
             case 5:
-                deleteStudent();
+                displayStudentsTable(studentArray, studentCount);
+                
+                printf("Enter the SID to Modify :");
+                scanf("%d",&target);
+                modifyStudent(studentArray,studentCount,target);
                 break;
+
             case 6:
-                system("cls");
-                printf("Exiting program. Goodbye!\n");
-                return 0;
+                printf("Exiting...\n");
+                break;
             default:
                 printf("Invalid choice. Please try again.\n");
         }
 
+        //pressAnyKeyToContinue(); // Wait for user input before returning to the menu
 
+    } while (choice != 6);
+
+    // Free dynamically allocated memory for student objects
+    for (int i = 0; i < studentCount; i++) {
+        free(studentArray[i]);
     }
 
     return 0;
 }
+
